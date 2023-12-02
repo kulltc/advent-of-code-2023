a8712b7218841ca90ad498861ee13d1279567567
kulltc <svenwitteveen@gmail.com>
2023-12-03 00:08:13 +0100

    shorter names

--- day_1.py
+++ day_1.py
@@ -1,7 +1,7 @@
 import re, inflect
-number_mapping = {**{inflect.engine().number_to_words(i): i for i in range(1, 10)}, **{str(i): i for i in range(1, 10)}}
+map = {**{inflect.engine().number_to_words(i): i for i in range(1, 10)}, **{str(i): i for i in range(1, 10)}}
 
-def find_number(string, direction):
-    return number_mapping[re.findall(re.compile(f"({('|'.join(number_mapping.keys())[::direction])})"), string[::direction])[0][::direction]]
+def find_number(string, dir):
+    return map[re.findall(re.compile(f"({('|'.join(map.keys())[::dir])})"), string[::dir])[0][::dir]]
 
 print(sum((find_number(line, 1) * 10 + find_number(line, -1) for line in open('day_1_input.csv'))))
